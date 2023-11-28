6922db2eadae969f9184d3ad9821b5f960bcc9aa
mac-omid <omidevil200@gmail.com>
2023-11-28 12:09:20 +0330

    [FEATURE] add news model

--- app/Models/News.php
+++ app/Models/News.php
@@ -5,7 +5,23 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class News extends Model
-{
-    use HasFactory;
+class News extends Model {
+
+	use HasFactory;
+
+	protected $fillable = [
+		'third_party_id',
+		'provider',
+		'title',
+		'category_id',
+		'body',
+		'image',
+		'url',
+		'author',
+		'published_at',
+	];
+
+	public function category() {
+		return $this->hasMany(NewsCategory::class);
+	}
 }
